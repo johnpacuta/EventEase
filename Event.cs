4c70ac0814cfbdd822b3776983942ff89b18bd8c
John Pacuta <johnpacuta@gmail.com>
2025-05-10 23:01:25 -0400

    added event form validation

--- Event.cs
+++ Event.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EventEase
 {
     public class Event
     {
         public int Id { get; set; }
+       [Required(ErrorMessage = "Name is required.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Date is required.")]
         public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "Location is required.")]
         public string Location { get; set; }
         public bool IsCreated { get; private set; }
 
--- obj/Debug/net9.0/EventEase.AssemblyInfo.cs
+++ obj/Debug/net9.0/EventEase.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fadc6a1eabc73c1f05f7f0b8d9d86264cf93cf14")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
